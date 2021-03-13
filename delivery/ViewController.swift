e3b52ed8aed2de61788f6ebc4aac248c340250dc
youssefes <ssh://git@my.domain.cz:2222/project-name>
2021-03-13 11:39:12 +0200

    Initial Commit

---
+++ delivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delivery
+//
+//  Created by youssef on 3/13/21.
+//  Copyright © 2021 youssef. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
